3238271b30a70ba5f9f9124be0f1ec04aa719689
Dzumanet <137023861+Dzumanet@users.noreply.github.com>
2023-11-06 10:53:37 +0100

    Directory Structure Created

---
+++ src/components/Select/UserFishSelection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UserFishSelection = () => {
+    return null;
+}
---
+++ src/views/Fish/FishList/FishList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FishList = () => {
+    return null;
+}
